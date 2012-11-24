86bbf42cdc7402ab45a5f40f2a4567b347f799ac
Alireza Savand <alireza.savand@gmail.com>
2012-11-24 21:00:01 +0400

    Configuration
    As in previous chapters, we start by configuring the Flask extensions
    that we will use. For the login system we will use two extensions, Flask-
    Login and Flask-OpenID. These are configured as follows (file
    app\__init__.py)
    
    The Flask-OpenID extension requires a path to a temp folder where files
    can be stored. For this we provide the location of our tmp folder.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import BASE_DIR
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(BASE_DIR, 'tmp'))
 
 from app import views, models
