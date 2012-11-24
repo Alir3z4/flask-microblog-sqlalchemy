65c426c1ec423a8805e9b7ac29cab2a53bd93f37
Alireza Savand <alireza.savand@gmail.com>
2012-11-24 19:19:33 +0400

    In this chapter we are going to write a few scripts that simplify the management of our database. Before we get into that let's review how a Python script is executed on the command line.
    This was a long tutorial. We have learned the basics of working with a
    database, but we haven't incorporated the database into our application
    yet. In the next chapter we will put all we have learned about databases
    into practice when we look at our user login system.
    
    we created our database and learned how to populate it with users and
    posts, but we haven't hooked up any of that into our app yet. And two
    chapters ago we've seen how to create web forms and left with a fully
    implemented login form.

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
+
 CSRF_ENABLED = True
 SECRET_KEY = "983b$5-=##@a4c8-363_=+6-11e2-b0e8-fc7516^@e0d394"
 
--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from app import app
 
 app.run(debug=True)
