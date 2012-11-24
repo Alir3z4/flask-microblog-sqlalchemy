59ba069d3c0d88ef1b71b55a1e9bff9be13aac44
Alireza Savand <alireza.savand@gmail.com>
2012-11-24 21:00:32 +0400

    Revisiting our User model
    The Flask-Login extension expects certain methods to be implemented in
    our User class. Outside of these methods there are no requirements for
    how the class has to be implemented.
    
    Below is our Flask-Login friendly User class (file app/models.py)...
    
    The is_authenticated method has a misleading name. In general this
    method should just return True unless the object represents a user that
    should not be allowed to authenticate for some reason.
    
    The is_active method should return True for users unless they are
    inactive, for example because they have been banned.
    
    The is_anonymous method should return True only for fake users that are
    not supposed to log in to the system.
    
    Finally, the get_id method should return a unique identifier for the
    user, in unicode format. We use the unique id generated by the database
    layer for this.

--- app/models.py
+++ app/models.py
@@ -13,6 +13,19 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.nickname
 
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
