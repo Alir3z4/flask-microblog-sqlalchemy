5ebba4934083e3a669809d6f234b82440a6ad3b2
Alireza Savand <alireza.savand@gmail.com>
2012-11-25 22:26:47 +0400

    Testing
    Testing
    
    Let's write a test for our unit testing framework that exercises all
    that we have built so far (file tests.py):
    ```
    class TestCase(unittest.TestCase):
        #...
        def test_follow(self):
            u1 = User(nickname = 'john', email = 'john@example.com')
            u2 = User(nickname = 'susan', email = 'susan@example.com')
            db.session.add(u1)
            db.session.add(u2)
            db.session.commit()
            assert u1.unfollow(u2) == None
            u = u1.follow(u2)
            db.session.add(u)
            db.session.commit()
            assert u1.follow(u2) == None
            assert u1.is_following(u2)
            assert u1.followed.count() == 1
            assert u1.followed.first().nickname == 'susan'
            assert u2.followers.count() == 1
            assert u2.followers.first().nickname == 'john'
            u = u1.unfollow(u2)
            assert u != None
            db.session.add(u)
            db.session.commit()
            assert u1.is_following(u2) == False
            assert u1.followed.count() == 0
            assert u2.followers.count() == 0
    ```
    After adding this test to the testing framework we can run the entire
    test suite with the following command:
    
    ./tests.py
    And if everything works it should say that all our tests pass.

--- tests.py
+++ tests.py
@@ -37,6 +37,31 @@ class TestCase(unittest.TestCase):
         assert nickname2 != "john"
         assert nickname2 != "john"
 
+    def test_follow(self):
+        u1 = User(nickname='john', email='john@example.com')
+        u2 = User(nickname='susan', email='susan@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        assert u1.unfollow(u2) is None
+        u = u1.follow(u2)
+        db.session.add(u)
+        db.session.commit()
+        assert u1.follow(u2) is None
+        assert u1.is_following(u2)
+        assert u1.followed.count() == 1
+        assert u1.followed.first().nickname == 'susan'
+        assert u2.followers.count() == 1
+        assert u2.followers.first().nickname == 'john'
+        u = u1.unfollow(u2)
+        assert u is not None
+        db.session.add(u)
+        db.session.commit()
+        assert u1.is_following(u2) == False
+        assert u1.followed.count() == 0
+        assert u2.followers.count() == 0
+
+
 if __name__ == '__main__':
     unittest.main()
 
